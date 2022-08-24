1e1ab705fe553bd5321581e4e249590496859a69
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-08-24 11:13:10 -0300

    Primeiro código php

--- ex001/ola.php
+++ ex001/ola.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Primeiro Codigo</title>
+    <style>
+        h2{
+            color: lightblue;
+            text-shadow: 1px 1px 2px gray; 
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Testando codigo PHP</h1>
+    <?php
+        echo "<h2>Ola, <br> Mundo.</h2>";
+    ?>
 </body>
 </html>

1802a3b24529e20c6029ce019d3684b781f9eba8
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-09-25 11:27:06 -0300

    finalizei  o exercicio 3

--- aula9/ex003.php
+++ aula9/ex003.php
@@ -10,18 +10,37 @@
         $n1 = number_format($_GET["n1"], 1);
         $n2 = number_format($_GET["n2"], 1);
         $med = number_format(($n1 + $n2) / 2, 1);
+        
         if($med > 7){
             $res = "APROVADO";
+            $medcolor = "green";
         }elseif($med < 7 && $med >= 5){
             $res = "RECUPERAÇÃO";
+            $medcolor = "#e5e52f";
         }else{
             $res = "REPROVADO";
+            $medcolor = "red";
         }
-        if($n1 > 7){$n1color = "green";}elseif($n1 < 7 && $med >5){$n1color = "yellow";}else{$n1color = "red";}
+
+        if($n1 > 7){$n1color = "green";} elseif($n1 >= 5){$n1color = "yellow";} else{$n1color = "red";}
+        
+        if($n2 > 7){$n2color = "green";} elseif($n2 >= 5 ){$n2color = "yellow";} else{$n2color = "red";}
     ?>
     <style>
         span#n1{
-            color = <?php echo $n1color?>;
+            color: <?php echo $n1color;?>;
+        }
+
+        span#n2{
+            color: <?php echo $n2color;?>;
+        }
+
+        span#med{
+            color: <?php echo $medcolor;?>;
+        }
+
+        span#res{
+            color: <?php echo $medcolor;?>;
         }
     </style>
 </head>
@@ -33,9 +52,10 @@
 
             
 
-            echo "Pela sua média a situação é <span class='res'>$res</span>;"
+            echo "Pela sua média a situação é <span id='res'>$res</span>;"
         ?>
     </div>
+    <a href="ex003.html">Voltar</a>
     <p>Aprendendo PHP</p>
 </body>
 </html>
